9ff44d0cd3a4746639472b2e4e17b3d394b96408
gamefensi <38190915+gamefensi@users.noreply.github.com>
2021-09-23 23:26:18 -0700

    2nd commit - added disable/enable & evaluateCode Function

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -23,4 +23,30 @@ document.getElementById("codes").innerHTML = generateCode();
 //Disable Button
 function disableButton(btnvalue) {
     document.getElementById("submit").disabled = btnvalue; //able/disable button
+    if (btnvalue == true) { //test if button is disabled or enabled
+        //set button and label color translucent
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        //set button and label color with no transparency
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
 }
+
+//listen to user input code
+var codebox = document.getElementById("codeentered"); //get textbox
+codebox.addEventListener("input", evaluateCode); //listen to code entered in textbox
+
+//run function if detected use had entered a character in textbox
+function evaluateCode() {
+    getCode = document.getElementById("codeentered").value; //get character entered
+    var charset1 = getCode.trim(); //remove any hidden characters entered
+    var charset2 = code.trim(); //remove any hidden characters generated
+    //test if code entered matches the number of generated characters
+    if (charset1.length == charset2.length && charset1 == charset2) {
+        disableButton(false); //if match, run the function to enable button
+    }
+}
+// //Activate function
+disableButton();
