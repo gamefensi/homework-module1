152f675b5d544d2cbdb22e8742ab53af4978091f
gamefensi <38190915+gamefensi@users.noreply.github.com>
2021-10-01 23:51:49 -0700

    revised jquery to detect if first-child & add css only through jquery - no hardcode classes

--- js/activity.js
+++ js/activity.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-    var notTD = $(".noSelect")
-    $("td").click(function(e) {
-        $(e.target).not(".noSelect").toggleClass("highlight");
+
+    $('td:contains("Not Available")').addClass("noSelect");
+    $('td').not('.noSelect').mouseover(function() {
+        $(this).css('cursor', 'pointer');
+    })
+    $("td").not("td:first-child").click(function(e) {
+        var active = $(e.target).not(".noSelect");
+        if (active.is(".selected")) {
+        active.css("background","#eeffe6");
+        active.css("font-weight","normal");
+        active.css("color","#000");
+        active.removeClass("selected");
+        } else {
+            active.css("background","#9ed034");
+            active.css("font-weight","bold");
+            active.css("color","#FFF");
+            active.addClass("selected");
+        }
     });
-});
+
+    });
