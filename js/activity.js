b9ee0e3cc13f84bb2867f46bf13a6837e6946122
John M <mahan.john@gmail.com>
2021-10-02 21:02:24 -0700

    added html,css,js for m2D3 HW

--- js/activity.js
+++ js/activity.js
@@ -1,22 +1,43 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     $('td:contains("Not Available")').addClass("noSelect");
-    $('td').not('.noSelect').mouseover(function() {
+    $('td').not('.noSelect').mouseover(function () {
         $(this).css('cursor', 'pointer');
     })
-    $("td").not("td:first-child").click(function(e) {
+    $("td").not("td:first-child").click(function (e) {
         var active = $(e.target).not(".noSelect");
         if (active.is(".selected")) {
-        active.css("background","#eeffe6");
-        active.css("font-weight","normal");
-        active.css("color","#000");
-        active.removeClass("selected");
+            active.css("background", "#eeffe6");
+            active.css("font-weight", "normal");
+            active.css("color", "#000");
+            active.removeClass("selected");
         } else {
-            active.css("background","#9ed034");
-            active.css("font-weight","bold");
-            active.css("color","#FFF");
+            active.css("background", "#9ed034");
+            active.css("font-weight", "bold");
+            active.css("color", "#FFF");
             active.addClass("selected");
         }
     });
 
+    $("td").click(function () { //user select a table data cell
+        var content = $(this).text(); //get content of cell
+
+        if (content != "Not Available") { //check if content does not contain a particular string
+            $(this).toggleClass("tdhighlight"); //add or remove class when cell is selected
+
+            if ($(this).hasClass("tdhighlight")) { //check if selected cell has class
+                $('#displaySelected').css("visibility", "visible"); //make display box visible
+                $('#displaySelected').css("margin-top", "2em"); // add spaces above display box
+                $('#result').append("<p>" + content + "</p>"); //add child element with contents of cell
+
+            } else { //if selected cell don't have class
+                $('#result p:contains(' + content + ')').remove(); //remove child element
+
+                if ($('#result').has('p').length == false) { //check if there are any child elements within parent
+                    $('#displaySelected').css("visibility", "hidden"); //make display box hidden
+                    $('#displaySelected').css("margin-top", "0"); //remove spaces above display box
+                }
+            }
+        }
     });
+});
