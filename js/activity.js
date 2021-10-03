a13dd3a02ac9cad80b066dd2b4748067240165d2
John M <mahan.john@gmail.com>
2021-10-02 21:21:35 -0700

    added index/eq to displaybox fn to extract cliff selection

--- js/activity.js
+++ js/activity.js
@@ -21,14 +21,15 @@ $(document).ready(function () {
 
     $("td").click(function () { //user select a table data cell
         var content = $(this).text(); //get content of cell
-
+        var colIndex = $(this).index();
+        var cliff = $("th").eq(colIndex).text();
         if (content != "Not Available") { //check if content does not contain a particular string
             $(this).toggleClass("tdhighlight"); //add or remove class when cell is selected
 
             if ($(this).hasClass("tdhighlight")) { //check if selected cell has class
                 $('#displaySelected').css("visibility", "visible"); //make display box visible
                 $('#displaySelected').css("margin-top", "2em"); // add spaces above display box
-                $('#result').append("<p>" + content + "</p>"); //add child element with contents of cell
+                $('#result').append("<p>" + content + " at " + cliff + "</p>"); //add child element with contents of cell
 
             } else { //if selected cell don't have class
                 $('#result p:contains(' + content + ')').remove(); //remove child element
