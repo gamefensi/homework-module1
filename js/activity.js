5c78c80076350e7524a02bc17d1b0cac92c337c0
John M <mahan.john@gmail.com>
2021-10-17 20:02:21 -0700

    made bg images responsive for all pages

--- js/activity.js
+++ js/activity.js
@@ -25,18 +25,19 @@ $(document).ready(function () {
         var cliff = $("th").eq(colIndex).text();
         if (content != "Not Available") { //check if content does not contain a particular string
             $(this).toggleClass("tdhighlight"); //add or remove class when cell is selected
+            $('#myModal').modal('show');
 
             if ($(this).hasClass("tdhighlight")) { //check if selected cell has class
-                $('#displaySelected').css("visibility", "visible"); //make display box visible
-                $('#displaySelected').css("margin-top", "2em"); // add spaces above display box
+                // $('#displaySelected').css("visibility", "visible"); //make display box visible
+                // $('#displaySelected').css("margin-top", "2em"); // add spaces above display box
                 $('#result').append("<p>" + content + " at " + cliff + "</p>"); //add child element with contents of cell
 
             } else { //if selected cell don't have class
                 $('#result p:contains(' + content + ')').remove(); //remove child element
 
                 if ($('#result').has('p').length == false) { //check if there are any child elements within parent
-                    $('#displaySelected').css("visibility", "hidden"); //make display box hidden
-                    $('#displaySelected').css("margin-top", "0"); //remove spaces above display box
+                    // $('#displaySelected').css("visibility", "hidden"); //make display box hidden
+                    // $('#displaySelected').css("margin-top", "0"); //remove spaces above display box
                 }
             }
         }
